0bc018616553c915b628fea889409f5e8a8727d5
Shohruhxon <mey1li2bo0yev3@gmail.com>
2020-01-16 20:36:47 +0500

    Fuck you to

---
+++ learn1.php
@@ -0,0 +1,38 @@
+<?php
+    echo 'Hello World!';
+    $nt = '2';
+    echo '<br>';
+    $abs = 'Salom';
+    var_dump($abs);
+    $array= [
+        'salom',
+        '123',
+        'array'=>[
+            9,
+            8
+        ]
+    ];
+    var_dump($array);
+
+    //binary if (shart operatori)
+    $abd = ($nt <= 2 ) ? 'we' : 'ew';
+    echo '<br>';
+    var_dump($abd);
+    echo $a =8;
+/*
+and -- &&
+or -- ||
+not -- !
+!= -- teng emas
+=== -- tipi va qiymati boyicha soliwtiradi
+*/
+
+//foreach loop example, bu siql faqat massiv uchun yaxwi 
+$arr = [1, 3, 5, 6, 2];
+foreach($arr as $index => $value){
+    echo $index ."=>".$value;
+    echo '<br>';
+}
+
+
+?>
